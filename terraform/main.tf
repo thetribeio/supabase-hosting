b70768d0531dcf1945eff5ace5be555ec24dbf86
Thomas Barusseau <thomas.barusseau@thetribe.io>
2024-12-16 14:56:52 +0100

    chore: add volume directories

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,15 +37,9 @@ resource "scaleway_instance_ip" "public_ip" {
   project_id = var.project_id
 }
 
-resource "scaleway_instance_volume" "data" {
-  project_id = var.project_id
-  size_in_gb = 20
-  type       = "b_ssd"
-}
-
 resource "scaleway_instance_server" "web" {
   project_id = var.project_id
-  type       = "STARDUST1-S"
+  type       = "DEV1-M"
   image      = "ubuntu_noble"
 
   name = "supabase"
@@ -54,10 +48,10 @@ resource "scaleway_instance_server" "web" {
 
   ip_id = scaleway_instance_ip.public_ip.id
 
-  additional_volume_ids = [scaleway_instance_volume.data.id]
+  additional_volume_ids = []
 
   root_volume {
-    size_in_gb = 10
+    size_in_gb = 30
   }
 }
 
