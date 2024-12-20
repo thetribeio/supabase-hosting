e632cb851037bd1c832c989f2a9851ecd89f0c3d
Thomas Barusseau <thomas.barusseau@thetribe.io>
2024-12-20 17:21:45 +0100

    feat: started working on a generic solution

---
+++ terraform/staging.tf
@@ -0,0 +1,57 @@
+output "staging_server_ip_address" {
+    value = "${scaleway_instance_ip.public_ip.address}"
+}
+
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+  }
+  required_version = ">= 0.13"
+
+  backend "s3" {
+    # Uncomment and edit the two lines below!
+    # bucket                      = "PROJECT_NAME-terraform-states"
+    # key                         = "PROJECT_NAME-supabase.tfstate"
+    region                      = "fr-par"
+    endpoints = {
+      s3 = "https://s3.fr-par.scw.cloud"
+    }
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
+}
+
+provider "scaleway" {
+  zone   = "fr-par-1"
+  region = "fr-par"
+
+  access_key = var.scw_access_key
+  secret_key = var.scw_secret_key
+
+  project_id = var.project_id
+}
+
+resource "scaleway_instance_ip" "public_ip" {
+  project_id = var.project_id
+}
+
+resource "scaleway_instance_server" "web" {
+  project_id = var.project_id
+  type       = "DEV1-M"
+  image      = "ubuntu_noble"
+
+  name = "supabase"
+
+  tags = []
+
+  ip_id = scaleway_instance_ip.public_ip.id
+
+  additional_volume_ids = []
+
+  root_volume {
+    size_in_gb = 30
+  }
+}
